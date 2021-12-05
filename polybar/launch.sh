82f9a12a05e65c53f98f3c89afca8cdf69a29513
Alexandru Mercan <alex.mercan2014@yahoo.com>
2021-12-05 13:36:42 +0200

    Support multiple monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,6 +8,12 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar_mainBar.log
 
-polybar mainBar 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mainBar &
+  done
+else
+  polybar --reload mainBar &
+fi
 
 echo "Bars launched..."
