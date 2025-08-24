90b32d192298ac473b899611f8105a22d7c8706b
Alex M <alexmercan2013@gmail.com>
2025-08-24 21:48:29 +0300

    update nvim conf

--- nvim/lua/config/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-local colorscheme = "catppuccin"
-
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha, amoled, catppuccino
-
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-    vim.notify("colorscheme " .. colorscheme .. " not found!")
-    return
-end
